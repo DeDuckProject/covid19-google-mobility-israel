be2d10cfe34f549173a69b945a38e9216ddd1f81
Iftach Yakar <fatach@gmail.com>
2020-11-10 19:55:51 +0200

    WIP correlation between calculated R and google mobility stats

--- plotting/mobilityRCorrelation.py
+++ plotting/mobilityRCorrelation.py
@@ -7,7 +7,12 @@ from dataHandling.googleMobilityData import getCountryData, allCategories
 from dataHandling.reproductionCalculation import getReproductionDf
 
 matplotlib.use('TkAgg')
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+
+# Date formatting for X-axis
+months = mdates.MonthLocator()  # every month
+days = mdates.DayLocator() # every day
 
 reproductionDf = getReproductionDf()
 [countryDf, subRegions1, subRegions2] = getCountryData('Israel')
@@ -15,8 +20,11 @@ reproductionDf = getReproductionDf()
 # reproductionDf['date'] = reproductionDf['date'].apply(lambda x: time.mktime(dt.datetime.strptime(x, "%Y-%m-%d").timetuple()))
 countryDf['date'] = countryDf['date'].apply(lambda x: x.strftime("%Y-%m-%d"))
 
-rollingMeanWindowSize = 3
+rollingMeanWindowSize = 7
 fig, ax = plt.subplots()
+
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_minor_locator(days)
 
 def processData(countryDf, reproductionDf):
     isNoSubRegion1 = countryDf['sub_region_1'] != countryDf['sub_region_1']  # filter only country non-region data
@@ -45,6 +53,17 @@ def processData(countryDf, reproductionDf):
 
     return [mergedDf, df_corr, df_p]
 
+def calculateCorrelation(mergedDf, category):
+    mergedDfFirstWave = mergedDf[mergedDf._get_index_resolvers()['date'] < '2020-06-15']
+    sorted = mergedDfFirstWave.sort_values(category)
+    # Plot:
+    x = sorted[category]
+    y = sorted['median']
+
+    # 2deg fit
+    px = np.polyfit(x, y, 2)
+    return np.poly1d(px)
+
 def plotCorrelation(mergedDf, corrDf, pDf, category):
     sorted = mergedDf.sort_values(category)
     # Plot:
@@ -67,28 +86,43 @@ def plotCorrelation(mergedDf, corrDf, pDf, category):
     plt.title('Correlation between Rt and {}'.format(category))
 
 def plotTimeseries(mergedDf, category):
+    yLabel = 'Rt'
     # Plot:
     x = mergedDf._get_index_resolvers()['date']
     y = mergedDf['median']
-    presence_transposed = (mergedDf[category])# / 20 + 3
+    # presence_transposed = (mergedDf[category])# / 20 + 3
 
-    label = '{}'.format(category)
-    ax.plot(x, y, label=label)
-    ax.plot(x, presence_transposed, label=label)
-    # m, b = np.polyfit(x, y, 1)
-    # ax.plot(x, m * x + b, '-')
+    ax.plot(x, y, label='source R')
+    # ax.plot(x, mergedDf[category], label=category)
+    # y += ' / Change in presence {}'.format(category)
+
     plt.xlabel('Date')
-    plt.ylabel('Rt / Change in presence {}'.format(category))
+    plt.ylabel(yLabel)
 
     plt.title('Correlation between Rt and {}'.format(category))
+    fig.autofmt_xdate()
 
+def projectR(mergedDf, category, p):
+    # mergedDf = mergedDf[mergedDf._get_index_resolvers()['date'] >= '2020-06-15'] # Filter projection
+    x = mergedDf._get_index_resolvers()['date']
+    y = p(mergedDf[category].rolling(window=rollingMeanWindowSize).mean())
+    # y = y.rolling(window=rollingMeanWindowSize).mean()
+    ax.plot(x, y, label='projection')
+    plt.title('Predicting Rt from "{}" data'.format(category))
+
+chosenCategory = allCategories[3]
 [mergedDf, corrDf, pDf] = processData(countryDf,reproductionDf)
-plotCorrelation(mergedDf, corrDf, pDf, allCategories[5])
-# plotTimeseries(mergedDf,allCategories[3])
+# plotCorrelation(mergedDf, corrDf, pDf, chosenCategory)
+# plotTimeseries(mergedDf,chosenCategory)
+
+# Try to preditct 2nd wave with 1st wave curve fit:
+curveFit = calculateCorrelation(mergedDf, chosenCategory)
+plotTimeseries(mergedDf,chosenCategory)
+projectR(mergedDf, chosenCategory, curveFit)
 
 # Put a legend to the right of the current axis
-# plt.subplots_adjust(right=0.75)
-# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize="large")
+plt.subplots_adjust(right=0.75)
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize="large")
 
 plt.grid(True)
 plt.show()
