68b917a1bc77e9aedc8b5bed26c1e02b7ae0bb2a
Iftach Yakar <fatach@gmail.com>
2021-02-03 21:28:46 +0200

    Added vaccinated plots

---
+++ dataHandling/vaccinatedData.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import datetime as dt
+import os
+
+# Read population data:
+
+
+# IMPORTANT: before running the script make sure you download the dataset and place in /data:
+# https://data.gov.il/dataset/f54e79b2-3e6b-4b65-a857-f93e47997d9c/resource/57410611-936c-49a6-ac3c-838171055b1f/download/vaccinated-per-day-2021-02-02.csv
+main_csv_filename = '../data/vaccinated-per-day-2021-02-02.csv'
+israelVaccinatedData = pd.read_csv(main_csv_filename)
+israelVaccinatedData = israelVaccinatedData.rename(columns={'VaccinationDate': 'date'})
+
+dateFetched = dt.datetime.fromtimestamp(os.path.getmtime(main_csv_filename)).strftime("%d/%m/%y")
+datagov_source_text_vaccinated = 'Israel covid-19 dataset.\nhttps://data.gov.il/dataset/covid-19 Accessed: {}'.format(
+    dateFetched)
+
+# Get rid of '<15' values:
+israelVaccinatedData['first_dose'] = israelVaccinatedData['first_dose'].replace(to_replace=r'^<15$', value='15', regex=True).astype(int)
+israelVaccinatedData['second_dose'] = israelVaccinatedData['second_dose'].replace(to_replace=r'^<15$', value='0', regex=True).astype(int)
+israelVaccinatedData.date = pd.to_datetime(israelVaccinatedData.date) # translate string date to date
+
+age_groups = ['0-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90+']
+# Population per age from https://www.cbs.gov.il/he/publications/doclib/2020/2.shnatonpopulation/st02_03.pdf :
+popForAgeThousand = [3263.9, 1267.5, 1179.6, 1068.8, 824.4, 729.5, 450.5, 219.6, 50.1]
+
+def getPopulationForAgeGroup(age_group):
+    return popForAgeThousand[age_groups.index(age_group)] * 1000
+
+def getVaccinatedData():
+    return [israelVaccinatedData, age_groups]
---
+++ plotting/vaccinatePlot.py
@@ -0,0 +1,75 @@
+import matplotlib
+import numpy as np
+import datetime
+from pandas.tseries.offsets import DateOffset
+
+from annotations import annotateEndLockdown2, annotate
+from colors import getColorByIndex
+from dataHandling.vaccinatedData import getVaccinatedData, getPopulationForAgeGroup, datagov_source_text_vaccinated
+
+matplotlib.use('TkAgg')
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+# Date formatting for X-axis
+months = mdates.MonthLocator()  # every month
+days = mdates.DayLocator() # every day
+
+# global style
+matplotlib.rcParams['axes.titlesize'] = 16
+matplotlib.rcParams['axes.labelsize'] = 12
+
+fig, ax = plt.subplots()
+# ax.xaxis.set_major_locator(months)
+# ax.xaxis.set_minor_locator(days)
+# ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+
+rollingMeanWindowSize = 7
+
+def plot_vaccinated_accumulation_per_age_group(vaccinatedData, age_groups, firstOrSecond, accum = True, percent = True):
+    if percent:
+        ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+        title = 'Israel - Vaccinated percent from age group'
+    else:
+        title = 'Israel - Vaccinated individuals by age group'
+
+    if firstOrSecond == 1:
+        title += '(first dose)'
+    else:
+        title += '(second dose)'
+
+    plt.title(title)
+
+    for ageGroup in age_groups:
+        onlyAgeGroup = vaccinatedData[vaccinatedData['age_group'] == ageGroup]
+        if firstOrSecond==1:
+            key = 'first_dose'
+        else:
+            key = 'second_dose'
+        if (accum):
+            onlyAgeGroup['first_dose_accum'] = onlyAgeGroup['first_dose'].cumsum()
+            onlyAgeGroup['second_dose_accum'] = onlyAgeGroup['second_dose'].cumsum()
+            key += '_accum'
+
+        x = onlyAgeGroup['date']
+        y = onlyAgeGroup[key]
+        if percent:
+            y = (y / getPopulationForAgeGroup(ageGroup)) * 100
+
+        ax.plot(x, y, label=ageGroup, linewidth=1)
+    plt.xlabel('Date')
+    plt.ylabel('Vaccinated for age group')
+
+[vaccinatedData, age_groups] = getVaccinatedData()
+
+plot_vaccinated_accumulation_per_age_group(vaccinatedData, age_groups, 1, False, True)
+# plot_vaccinated_accumulation_per_age_group(vaccinatedData, age_groups, 2, False)
+
+# Put a legend to the right of the current axis
+plt.subplots_adjust(right=0.75)
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize="large")
+plt.figtext(0.2, 0.05, datagov_source_text_vaccinated, ha="center")
+
+plt.grid(True)
+plt.show()
